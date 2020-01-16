b85c4ef6c05f7b392074dfb76ca406bb69b16b99
Lukasz Kurek <lukascoorek@gmail.com>
2020-01-16 12:11:19 +0100

    adding styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,52 @@
 import React from "react"
 import { Link } from "gatsby"
+import headerStyle from "./header.module.scss"
 
 const Header = () => {
   return (
-    <header>
-      <h1>Blog</h1>
+    <header className={headerStyle.header}>
+      <h1 className={headerStyle.title}>Lukasz Kurek</h1>
       <nav>
-        <ul>
+        <ul className={headerStyle.navList}>
           <li>
-            <Link to="/">Home</Link>
+            <Link
+              to="/"
+              className={headerStyle.navItem}
+              activeClassName={headerStyle.navItemActive}
+            >
+              {" "}
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link
+              to="/contact"
+              className={headerStyle.navItem}
+              activeClassName={headerStyle.navItemActive}
+            >
+              {" "}
+              Contact
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link
+              to="/about"
+              className={headerStyle.navItem}
+              activeClassName={headerStyle.navItemActive}
+            >
+              {" "}
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link
+              to="/blog"
+              className={headerStyle.navItem}
+              activeClassName={headerStyle.navItemActive}
+            >
+              {" "}
+              Blog
+            </Link>
           </li>
         </ul>
       </nav>
